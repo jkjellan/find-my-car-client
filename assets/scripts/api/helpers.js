5fa3e45d515de97cca070345efcbb2abab7cd3fa
jkjellan <jkjellan@gmail.com>
2017-06-01 23:53:48 -0400

    Modularizes map API functions, and promisifies geo location
    HTML geolocation function to return current location of user
    device now returns a promise.  The return of the promise kicks off
    a chain of google map api functionality. I have started refactoring
    google api functionality, creating stand-alone functions in helpers.js,
    which I invoke within events.js.  I still can't get the maps to work
    properly in Safari localhost, but it works in Safari when deployed,
    so not worrying about it for now.

--- assets/scripts/api/helpers.js
+++ assets/scripts/api/helpers.js
@@ -1,5 +1,130 @@
 'use strict'
 
-module.exports = {
+const initializeMapWithGeo = function (position) {
+  console.log('initializeMapWithGeo, position is', position)
+  const latLng = {lat: position.coords.latitude, lng: position.coords.longitude}
+  const map = new google.maps.Map(document.getElementById('map'), {zoom: 17, center: latLng})
+  return map
+}
 
+const initializeMapNoGeo = function () {
+  console.log('initializeMapNoGeo Ran')
+  let latLng = {lat: 42.3601, lng: 71.0589}
+  const map = new google.maps.Map(document.getElementById('map'), {zoom: 17, center: latLng})
+  return map
+}
+
+const placeMarker = function (position, map) {
+  console.log('about to place a marker to position, map', position, map)
+  const latLng = {lat: position.coords.latitude, lng: position.coords.longitude}
+  const marker = new google.maps.Marker({
+    position: latLng,
+    draggable: true,
+    title: 'Last parking spot'
+    // animation: google.maps.Animation.BOUNCE
+  })
+  marker.setMap(map)
+  return marker
+}
+
+const attachMarkerHandlers = function (marker) {
+  google.maps.event.addListener(marker, 'dragend', function (e) {
+  console.log('dragging ended')
+  document.getElementById('current').innerHTML =
+  '<p>Marker dropped: Current Lat: ' + e.latLng.lat().toFixed(3) +
+  ' Current Lng: ' + e.latLng.lng().toFixed(3) + '</p>'
+  })
+
+  // google.maps.event.addListener(marker, 'dragstart', function (e) {
+  // console.log('dragging ended')
+  // document.getElementById('current').innerHTML =
+  // '<p>Marker dropped: Current Lat: ' + e.latLng.lat().toFixed(3) +
+  // ' Current Lng: ' + e.latLng.lng().toFixed(3) + '</p>'
+  // })
+}
+
+const loadMapOverlays = function (map) {
+  let myControl = document.getElementById('current')
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(myControl)
+}
+
+const getCurrentLocation = function () {
+  const options = {
+    // enableHighAccuracy: false,
+    // timeout: Infinity,
+    // maximumAge: 0
+    enableHighAccuracy: false,
+    timeout: 10000,
+    maximumAge: 60000
+  }
+
+  return new Promise(function (resolve, reject) {
+    // if browser doesn't support navigator.geolocation, those values will
+    // be falsey, and since !falsey is true, the if will trigger and reject
+    // the promise, passing an error, saying that Geolocation is not supported
+    if (!navigator.geolocation || !navigator.geolocation.getCurrentPosition) {
+      return reject(new Error('Geolocation not supported by browser'))
+    }
+    // otherwise, getCurrentPosition will resolve, passing coordinates to the first
+    // callback, or reject, passing and error to second callback.
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  })
+}
+
+// const geoLocation = function () {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//       console.log('navigator geolocation is', position)
+//       const pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       }
+//       if (pos) {
+//         console.log('returning pos of', pos)
+//         return pos
+//       }
+//     }, function (err) {
+//       console.warn(`ERROR(${err.code}): ${err.message}`)
+//     })
+//   } else {
+//     // Browser doesn't support Geolocation
+//     console.log('If browser does not support geolocation, need to show map centered on Boston and tell the user to search for current location')
+//     // handleLocationError(false, infoWindow, map.getCenter())
+//   }
+// }
+
+const displayInfoWindow = function () {
+  let infoWindow = new google.maps.InfoWindow
+  infoWindow.setPosition(pos)
+  infoWindow.setContent('Location found.')
+  // infoWindow.open(map)
+  // map.setCenter(pos)
+}
+
+const handleLocationError = function (browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos)
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map)
+}
+
+// marker.setMap(null)
+// marker = null
+
+// If you wish to manage a set of markers, you should create an array to hold the markers.
+// Using this array, you can then call setMap() on each marker in the array in turn when
+// you need to remove the markers. You can delete the markers by removing them from the map
+// and then setting the array's length to 0, which removes all references to the markers.
+
+module.exports = {
+  initializeMapWithGeo,
+  initializeMapNoGeo,
+  placeMarker,
+  attachMarkerHandlers,
+  loadMapOverlays,
+  // geoLocation,
+  getCurrentLocation,
+  displayInfoWindow,
+  handleLocationError
 }
