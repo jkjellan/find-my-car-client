52b388fe3d8e1f83005a25bfda8d7b1680e035fd
jkjellan <jkjellan@gmail.com>
2017-06-08 12:45:15 -0400

    Adds remaining alerts to my hide function in showAlerts.js

--- assets/scripts/map/helpers/showAlerts.js
+++ assets/scripts/map/helpers/showAlerts.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const hideAlerts = () => {
+  $('#sign-out-failure-alert-id').hide()
+  $('#new-parking-spot-failure-alert-id').hide()
+  $('#get-parking-spots-failure-alert-id').hide()
+  $('#get-parking-spot-failure-alert-id').hide()
+
   $('#no-geo-alert-id').hide()
   $('#update-success-alert-id').hide()
   $('#delete-success-alert-id').hide()
